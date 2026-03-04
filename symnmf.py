8d3221172a2ee21099e71b49b4f5553fe671a5fb
LBornstein <lior.bornstein@gmail.com>
2026-03-04 17:28:51 +0200

    almost finished the python

--- symnmf.py
+++ symnmf.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sys
-import math
+import symnmf
 
 def main():
     np.random.seed(1234)
@@ -14,40 +14,55 @@ def main():
     file_name = sys.argv[3]
 
     points_list = parse_points()
+    n = len(points_list)
+    d = len(points_list[0])
 
     max_iter = 300
     epsilon = 1e-4
 
-    if goal == 'symnmf'():
-        handle_symnmf
+    if goal == 'symnmf':
+        W = symnmf.norm(points_list, n ,d)
+        H = init_H(W, k)
+        M = symnmf.symnmf(H, W, n, k)
     elif goal == 'sym':
-        handle_sym
+        M = symnmf.sym(points_list, n, d)
     elif goal == 'ddg':
-        handle_ddg
+        M = symnmf.ddg(points_list, n, d)
     elif goal == 'norm':
-        handle_norm
+        M = symnmf.norm(points_list, n, d)
     else:
         print("An Error Has Occurred")
         sys.exit(1)
     
+    output_mat(M)
     exit(0)
 
+def calculate_matrix_avg(M):
+    sum = 0
+    for i in range(len(M)):
+        for j in range(len(M[0])):
+            sum += M[i][j]
+    return sum / (len(M)**2)
+    
+def init_H(W, k):
+    n = len(W)
+    m = calculate_matrix_avg(W)
+    interval = 2 * ((m / k) ** 0.5)
+    H = [[np.random.uniform(0.0, interval) for col in range(k)] for row in range(n)]
+    return H
 
 def parse_points(file_name):
     pass
 
-
-def handle_symnmf():
-    pass
-def handle_sym():
-    pass
-def handle_ddg():
-    pass
-def handle_norm():
-    pass
-
 def output_mat(m):
-    pass
+    s = ""
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            s = s + "," + m[i][j]
+        s = s[:-1] + "\n"
+    s = s[:-1]
+    print(s)
+
 
 
 if (__name__=="__main__"):
