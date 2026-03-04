0c3db42ed4fdc68a231497848452bc51aba28aa2
zivpeltz <zivpeltz@gmail.com>
2026-03-04 18:25:00 +0200

    "added MakeFile and setup.py"

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import Extension, setup
+
+module = Extension("symnmf",
+                   sources=['symnmfmodule.c', 'symnmf.c'])
+
+setup(name='symnmf',
+      version='1.0',
+      description='Python wrapper for C symNMF engine',
+      ext_modules=[module])
